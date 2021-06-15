4a8344a87f1cdc22edfa489dfee341696181ee72
rajt26 <thakkarraj700@gmail.com>
2021-06-15 13:25:14 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,32 +1,28 @@
-import './App.css';
-import Register from'./components/Register/Register'
-import Showdata from './components/ShowData/ShowData';
-import Mobx from './components/MobX/Mobx'
-import { useState, useEffect } from 'react';
-import UserStore from './UserStore'
+import "./App.css";
+import Register from "./components/Register/Register";
+import Showdata from "./components/ShowData/ShowData";
+import Mobx from "./components/MobX/Mobx";
+import { useState, useEffect } from "react";
+import UserStore from "./UserStore";
 
 // import "bootstrap/dist/css/bootstrap.css";
 
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-} from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 const App = () => {
+  const store = new UserStore();
+  console.log("store", store);
+  const [editItem, setEditItem] = useState(null);
+  const [list, setList] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-  const store =new UserStore()
-  const [ editItem ,setEditItem] = useState(null);
-  const [ list ,setList] = useState([]);
-  const [ loading ,setLoading] = useState(true);
-
-  const onEdit = (item) =>{
-    setEditItem(item)
-    console.log(item)
-  }
+  const onEdit = (item) => {
+    setEditItem(item);
+    console.log(item);
+  };
 
   const getUserData = async () => {
-    setLoading(true)
+    setLoading(true);
     try {
       const res = await fetch("http://localhost:4000/find", {
         method: "GET",
@@ -44,23 +40,27 @@ const App = () => {
   };
   useEffect(() => {
     getUserData();
-  },[]);
+  }, []);
 
   return (
     <>
-    <Router>
-    <Switch>
-    <Route exact path="/">
-    <Register editItem={editItem} clear={()=>setEditItem(null)}  />
-    {loading ? <p>Loading .....:)</p>:<Showdata list={list} onEdit={onEdit}  />}
-   </Route>
-    <Route path="/mobx">
-    <Mobx store={store}/>
-    </Route>
-    </Switch>
-    </Router>
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Register editItem={editItem} clear={() => setEditItem(null)} />
+            {loading ? (
+              <p>Loading .....:)</p>
+            ) : (
+              <Showdata list={list} onEdit={onEdit} />
+            )}
+          </Route>
+          <Route path="/mobx">
+            <Mobx store={store} />
+          </Route>
+        </Switch>
+      </Router>
     </>
   );
-  }
+};
 
 export default App;
